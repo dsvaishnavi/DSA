ce74206e22662265def7a4696433497bf15dad33
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-03-07 20:48:20 +0530

    removenum

---
+++ removenum.py
@@ -0,0 +1,12 @@
+nums=[0,1,2,2,3,0,4,2]
+val=2
+
+        
+        
+i = 0  # Pointer to track the position for valid elements
+for num in nums:
+    if num != val:
+        nums[i] = num  # Overwrite nums[i] with valid element
+        i += 1  # Move pointer forward
+
+print(i) 
