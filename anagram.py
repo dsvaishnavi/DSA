8db9d73655f6822b855dd1e3f44716406a892b94
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-03-12 22:52:12 +0530

    anagram

--- anagram.py
+++ anagram.py
@@ -1,5 +1,14 @@
 s="nagaram"
 t="anagram"
+hash={}
 
-if len(s) != len (t):
+
+if len(s) != len(t):
     print(False)
+else:
+    for i,n in enumerate(s):
+        hash[i] = n
+    print(hash)
+    
+
+    
