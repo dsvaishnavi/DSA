49b095358341c11391e5357998c3ef4e2b151a8b
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-03-18 22:44:58 +0530

    duplicate remove

--- linkedlist/removeduplicate.py
+++ linkedlist/removeduplicate.py
@@ -1,35 +1,37 @@
 class Node:
-    def __init__(self,val=0,next=None):
-        self.val=val
-        self.next=next
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-#assign value
-    
-node1=Node(10)
-node2=Node(10)
-node3=Node(20)
+# Create linked list nodes
+node1 = Node(10)
+node2 = Node(10)
+node3 = Node(20)
+node4 = Node(30)
+node5 = Node(20)
 
-#link it
+# Link nodes together
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node5
 
-node1.next=node2
-node2.next=node3
+# Remove duplicates (even if not consecutive)
 
-current=node1
+current = node1
+
 while current:
-    check=current.next
-    while check:
-        check.val==current.val
-        print("duplicate")
+    runner = current  # A second pointer to check future nodes
+    while runner.next:  # Traverse remaining nodes
+        if runner.next.val == current.val:  # If duplicate is found
+            runner.next = runner.next.next  # Remove the duplicate node
+        else:
+            runner = runner.next  # Move to next node
+    current = current.next  # Move main pointer forward
 
-
-
-def PrintList(head):
-    current=head
-    while current:
-        print(current.val,end="-->")
-        current=current.next
-    print("end")
-    if head == current:
-        head=current.next
-PrintList(node1)
-
+# Print the linked list without duplicates
+current = node1
+while current:
+    print(current.val, end=" --> ")
+    current = current.next
+print("None")
