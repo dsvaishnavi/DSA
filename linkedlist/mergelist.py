3a05abaa0c25105a31fac21a1cc60dc72974bd92
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-03-24 22:45:15 +0530

    new

---
+++ linkedlist/mergelist.py
@@ -0,0 +1,24 @@
+class List:
+    def __init__(self,val=0,next=None):
+        self.val=val
+        self.next=next
+
+#assign value
+    
+node1=List(10)
+node2=List(10)
+node3=List(20)
+
+#link it
+
+node1.next=node2
+node2.next=node3
+
+def PrintList(head):
+    current=head
+    while current:
+        print(current.val,end="-->")
+        current=current.next
+    print("end")
+PrintList(node1)
+
