f341008a827cf917eddce780f3a64736aa2551b6
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-03-24 22:53:59 +0530

    traverselist

--- linkedlist/mergelist.py
+++ linkedlist/mergelist.py
@@ -1,24 +1,18 @@
-class List:
+
+
+class Listnode:
     def __init__(self,val=0,next=None):
         self.val=val
         self.next=next
-
-#assign value
-    
-node1=List(10)
-node2=List(10)
-node3=List(20)
-
-#link it
-
-node1.next=node2
-node2.next=node3
-
-def PrintList(head):
-    current=head
-    while current:
-        print(current.val,end="-->")
-        current=current.next
-    print("end")
-PrintList(node1)
-
+def Traverse(head):
+        temp=head
+        while temp:
+            print(temp.val ,end="-->")
+            temp=temp.next
+        print("None")
+        
+head=Listnode(10)
+head.next=Listnode(1)
+head.next.next=Listnode(2)
+Traverse(head)
+        
