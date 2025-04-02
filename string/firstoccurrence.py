dda08bbb3271cd3a8b0d8a7af696d9183445d384
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-02 22:33:03 +0530

    occureence

--- string/firstoccurrence.py
+++ string/firstoccurrence.py
@@ -1,7 +1,12 @@
-haystack = "sadbutsad",
-needle = "sad"
-hashmap={}
+haystack = "hello"
+needle = "ll"
 
-for i in needle:
-    if i in haystack:
-        hashmap
+n= len (needle)
+for i,j in enumerate (haystack):
+    f=haystack[i:i+n]
+    if f==needle:
+        print(i)
+        break
+else:
+    print(-1)
+
