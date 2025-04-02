f917be9585ab2d8e6112ba52b14e2ca509b149ef
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-02 22:37:40 +0530

    occurrence

--- string/firstoccurrence.py
+++ string/firstoccurrence.py
@@ -1,7 +1,8 @@
-haystack = "sadbutsad"
+haystack = "sabutsad"
 needle = "sad"
 n= len (needle)
-for i,j in enumerate (haystack):
+
+for i in range (len(haystack)):
     f=haystack[i:i+n]
     if f==needle:
         print(i)
