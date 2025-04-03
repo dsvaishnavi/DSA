766cf81d6ba0beb367f91a8664458322239dab13
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-03 22:43:36 +0530

    new

--- array/largestnumber.py
+++ array/largestnumber.py
@@ -1,18 +1,27 @@
 nums = [10,2]
 null=""
-
+null2=""
 for i in nums:
     i=str(i)
     null+=i
 
-p=len(null)-1
 
-for i in null:
-    if i < p:
-        null[i],null[p]=null[p],null[i]
-    print(True)
+new=list(null)
+
+leng=len(new)
+
+for i in range(leng):
+    for j in range(0,leng-i-1):
+        if new[j] < new[j+1]:
+            new[j],new[j+1]=new[j+1],new[j]
 
 
+
+for p in new:
+    null2+=p
+print(null2)
+    
+            
     
 
     
