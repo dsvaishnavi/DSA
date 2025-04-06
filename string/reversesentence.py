7bc6aaf0364e5cbf3e3f4024fee90a7946629e1b
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-06 20:22:50 +0530

    reversed

--- string/reversesentence.py
+++ string/reversesentence.py
@@ -1,28 +1,34 @@
-s="race a car"
+s="A man, a plan, a canal: Panama"
 s=s.lower()
 null=""
 
 for i in s:
     if i.isalnum():
         null +=i
-print(null)
 
-cleaned=null
-null=list(null)
-
-i=0
-j=len(null)-1
-
-while i < j:
-    null[i],null[j] = null[j],null[i]
-    i +=1
-    j -= 1
-result=""
-for char in null:
-    result+=char
-    
-if result==cleaned:
-    print(True)
-else:
+null2=null[::-1]
+print(null2)
+if null != null2:
     print(False)
+else:
+    print(True)
+
+# cleaned=null
+# null=list(null)
+
+# i=0
+# j=len(null)-1
+
+# while i < j:
+#     null[i],null[j] = null[j],null[i]
+#     i +=1
+#     j -= 1
+# result=""
+# for char in null:
+#     result+=char
+    
+# if result==cleaned:
+#     print(True)
+# else:
+#     print(False)
 
