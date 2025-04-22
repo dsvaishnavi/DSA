a339a0ec2b6a78662f3ee0baf4beaa33609539a6
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-22 22:25:47 +0530

    add

--- string/containvowel.py
+++ string/containvowel.py
@@ -1,1 +1,23 @@
-word=""
+s ="abaccbc"
+hash={}
+
+for i in s:
+    if i in hash:
+        hash[i] +=1
+    else:
+        hash[i] = 1
+val=list(hash.values())
+   
+same = True
+first = val[0]
+
+
+for v in val:
+    if v != first:
+        same =False
+print(same)
+    
+
+
+
+
