215cd15099391e74db22a70190a01c7cfd28b16d
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-23 20:07:10 +0530

    add

--- string/reverse_prefix.py
+++ string/reverse_prefix.py
@@ -1,3 +1,13 @@
 word="abcdefd"
 ch="d"
+v=""
 
+index = word.find(ch)
+if index != -1:
+    result=word[:index +1]
+    result=result[::-1]+word[index +1:]
+    print(result) 
+else:
+    r
+        
+    
