1b8fb3180e53ed6ca3f5d5519c954ae6d281b08a
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-25 22:50:25 +0530

    add

--- string/ascending_num.py
+++ string/ascending_num.py
@@ -1,14 +1,9 @@
 s="1 box has 13 blue 4 red 6 green and 12 yellow marbles"
 null=[]
-
 v=s.split()
-
 for i,j in enumerate (v):
     if j.isdigit():
         null.append(int(j))
-print(null)
-    
-
 flag = True
 for b in range(len(null)-1):
     if null[b]>null[b+1]:
