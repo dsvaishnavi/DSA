b32bc3e60c0e902029bd527acd400287a5e40431
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-05-04 11:49:47 +0530

    dsa

--- array/domino_pair.py
+++ array/domino_pair.py
@@ -1,1 +1,14 @@
-dominoes = [[1,2],[2,1],[3,4],[5,6]]
+dominoes = [[1,2],[3,4],[5,6]]
+pairs=0
+hash={}
+for d in dominoes:
+   key=tuple(sorted(d))
+   
+   if key in hash:
+       pairs += hash[key]                       
+       hash[key] += 1   
+   else:
+       hash[key] =1
+print(pairs)
+      
+  
