0ed0ef430fd1d102f8db5f911306070baa919124
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-01 14:04:30 +0530

    add

--- array/two-pointer/longestPalindrome5.py
+++ array/two-pointer/longestPalindrome5.py
@@ -1,1 +1,11 @@
 s = "babad"
+new_s=""
+
+
+for i in range(len(s)):
+    for j in range(i+1,len(s)+1):
+        sub=s[i:j]
+        if sub == sub[::-1]:
+            if len(sub)>len(new_s):     #longest so checking length
+                new_s=sub
+print(new_s)
