38485eab66a98b264d27164a5ff44f22f1628666
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-01 13:19:44 +0530

    add

--- array/two-pointer/reversestring.py
+++ array/two-pointer/reversestring.py
@@ -1,10 +1,10 @@
 s=["h","e","l","l","o","e","t"]
 
-left=0
+left = 0
 right=len(s)-1
 
 while left < right:
-    s[left],s[right]=s[right],s[left]
-    left +=1
-    right -=1
+    s[left],s[right] =s[right],s[left]
+    left+=1
+    right-=1
 print(s)
