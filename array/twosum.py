978b2be68a164f659bb7052214edfb329149c1bb
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-06 20:20:26 +0530

    add

--- array/twosum.py
+++ array/twosum.py
@@ -1,17 +1,12 @@
 nums=[3,2,4]
 target=6
 hashmap={}
-
-for i,num in enumerate (nums):
-    minus= target-num
+for i,num in enumerate (nums):  
+    minus=target-num
     if minus in hashmap:
         print(hashmap[minus],i)
     else:
         hashmap[num]=i
-        
-   
-        
-
 
     
 
