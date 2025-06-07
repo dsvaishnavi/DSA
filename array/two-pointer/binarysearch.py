852038d961520c32e2320b33ac96fb78dcf7d0ac
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-07 09:24:28 +0530

    binary search

--- array/two-pointer/binarysearch.py
+++ array/two-pointer/binarysearch.py
@@ -1,2 +1,17 @@
 nums = [-1,0,3,5,9,12]
 target = 9
+left=0
+right=len(nums)-1
+
+while (left <= right):
+    mid=(left+right)//2                 #precedence is important
+    
+    if target == nums[mid]:
+        print(mid)
+        break
+    elif target > nums[mid]:
+        left=mid+1
+    elif target < nums[mid]:
+        right=mid-1
+else:
+    print(-1)
