ad0758326e5240044ac677dc9673fd9f0a8974e6
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-16 18:09:56 +0530

    new

---
+++ array/two-pointer/firstmissing.py
@@ -0,0 +1,6 @@
+# nums = [1,2,0]
+# # Output: 3
+# n=len(nums)
+
+# for i in range(n):
+    
--- string/firstoccurrence.py
+++ string/firstoccurrence.py
@@ -1,12 +1,5 @@
 haystack = "sabutsad"
 needle = "sad"
-n= len (needle)
 
-for i in range (len(haystack)):
-    f=haystack[i:i+n]
-    if f==needle:
-        print(i)
-        break
-else:
-    print(-1)
+
 
