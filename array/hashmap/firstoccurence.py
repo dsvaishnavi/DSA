b62e4fdf03153d1073cbaa50f1d9ae875e764888
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-06-23 10:41:16 +0530

    add

--- array/hashmap/firstoccurence.py
+++ array/hashmap/firstoccurence.py
@@ -1,8 +1,10 @@
 s="leetcode"
 hashmap={}
-for i in range (len(s)):
-    if i not in hashmap:
-        hashmap.add(i)
+for i in s:
+    hashmap[i] = hashmap.get(i, 0) + 1
+for i,n in enumerate(s):
+    if hashmap[n]==1:
+        print(i)
     else:
-        hashmap.get(i,0)+1
-        print(hashmap)
+        print(-1)   
+        
